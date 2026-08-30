19875ac3521f0e4d3d8dba637fc6303990b793d1
Anchal Jangid <anchaljangid817@gmail.com>
2026-08-30 15:30:05 +0530

    Add first Python practice
    Add first Python practice

---
+++ 01_basics.py
@@ -0,0 +1,11 @@
+# My first Python practice
+
+numbers = [10, 20, 20, 30, 40, 40, 50]
+
+unique_numbers = set(numbers)
+unique_numbers = list(unique_numbers)
+unique_numbers.sort()
+
+print("Original list:", numbers)
+print("Unique sorted list:", unique_numbers)
+print("Sum:", sum(unique_numbers))
