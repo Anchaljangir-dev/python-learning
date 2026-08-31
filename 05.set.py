f236d32792898912c8136d6c9b0172d425f65347
Anchal Jangir <anchaljangid817@gmail.com>
2026-08-31 12:29:05 +0530

    set methods

---
+++ 05.set.py
@@ -0,0 +1,8 @@
+set1 = {45,67,3,24,67,}
+print(sum(set1)) # we can sum all number in sets
+# we can sort a set by converting it into a list
+unique_set = list(set1)
+unique_set.sort()
+print(unique_set)
+# we can find highest, second highest by using index in it 
+print(unique_set[-1])
