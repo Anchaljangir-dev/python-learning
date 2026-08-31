0af92a6407aa67a96ebb7e5bc83c83398b97c54a
Anchal Jangir <anchaljangid817@gmail.com>
2026-08-31 12:20:10 +0530

    tuple unpacking

---
+++ 04.tuple.py
@@ -0,0 +1,5 @@
+# unpacking a tuple
+t1 = ("anchal", 22, "chitawa", "rajasthan")
+name, age, village, state, = t1
+# if you ask name it automatically show anchal it is called tuple unpacking 
+print(name)
