f04cda339353a832a155225536203e137f79bc67
Anchal Jangir <anchaljangid817@gmail.com>
2026-08-31 12:55:43 +0530

    while loops

---
+++ 07.whileloops.py
@@ -0,0 +1,20 @@
+# while loop run only if given condition is true
+i = 1
+while (i<6):
+    print(i)
+    i +=1
+
+# write a prgram for a name to print many given times
+i = 0
+while (i<6):
+    print("Anchal")
+    i = i + 1
+
+
+# to write a program of write list using while
+L = ["Anchal", "False", "girl", "maturity"]
+i = 0
+while (i<len(L)):
+    print(L[i])
+    i +=1
+    
