bee0e888ed2e5f02d6ea4fe7fee5fc33856e59a9
Anchal Jangir <anchaljangid817@gmail.com>
2026-09-01 13:05:49 +0530

    python loops

--- 09_practiceset_loops.py
+++ 09_practiceset_loops.py
@@ -60,3 +60,6 @@ for i in range(1,n+1):
     print("")
 
 # write a program to multiply a table in reverse order
+n = int(input("enter a number"))
+for i in range(1, 11):
+    print(f"{n} X {11 - i} = {n} * {11-i}") 
