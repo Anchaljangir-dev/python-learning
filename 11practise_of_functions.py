a271effe6983c6781b02965141dd7b4825b03359
Anchal Jangir <anchaljangid817@gmail.com>
2026-09-09 14:29:44 +0530

    Functions practise

--- 11practise_of_functions.py
+++ 11practise_of_functions.py
@@ -15,18 +15,52 @@
 # print(cal())
 # To find which number is greatest using function 
 
-def greatest(a, b, c, d):
-    if (a>b or a>c or a>d):
-        return a 
-    elif (b>a or b>c or b>d):
-        return b
-    elif (c>a or c>b or c>d):
-        return c
-    elif (d>a or d>b or d>c):
-        return d 
-a =  int(input("enter a number"))
-b =  int(input("enter a number"))
-c =  int(input("enter a number"))
-d =  int(input("enter a number"))
+# def greatest(a, b, c, d):
+#     if (a>b or a>c or a>d):
+#         return a 
+#     elif (b>a or b>c or b>d):
+#         return b
+#     elif (c>a or c>b or c>d):
+#         return c
+#     elif (d>a or d>b or d>c):
+#         return d 
+# a =  int(input("enter a number"))
+# b =  int(input("enter a number"))
+# c =  int(input("enter a number"))
+# d =  int(input("enter a number"))
 
-print(greatest(a , b, c , d))
+# print(greatest(a , b, c , d))
+# def fun():
+#     C = int(input("enter the temperature "))
+#     return (C * 9/5) +32
+# a = fun()
+# print(round(a, 2) ,"F")
+# end ="" to prevent a new line 
+# def sum(n):
+#     if n ==1:
+#         return 1
+#     return sum(n-1) + n 
+
+# print(sum(4))
+
+# # print reverse star pattern 
+# def pattern(n):
+#     if n ==0:
+#         return
+#     print("*" *n)
+#     pattern(n-1)
+# pattern(8)
+# write a program to change inches to centimeters 
+# def inch_to_cen(inches):
+#     return inches*2.54
+# n = int(input("enter the value in inches: "))
+# print(f"The corresponding value in cms i : {inch_to_cen(n)}")
+def rem(l, word):
+    n = []
+    for item in l:
+        if not(item ==word):
+            n.append(item.strip(word))
+    return n 
+l = ["anchal", "herry", "potter", "al"]
+print(rem(l , "al"))
+# to strip and remove a character and a word on same time 
