839e216a5d182b637e1e7e271a4a74589fb1df9f
Anchal Jangir <anchaljangid817@gmail.com>
2026-09-10 15:16:34 +0530

    definition of oop

---
+++ oop.py
@@ -0,0 +1,4 @@
+# object oriented programming 
+'''solving problem be creating a object this is the most popular approaches in programming
+this concept focusing on reusable code(DRY Principle)
+CLASS is a bluprint of creating objects  class later converted into object '''
